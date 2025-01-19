f4c4a7c62a8f87d9dd6976eea9566f93623c66b8
strawhatrag <sreeragbharathan791@gmail.com>
2025-01-19 22:48:47 +0530

    second commit

--- src/Conditions.java
+++ src/Conditions.java
@@ -1,2 +1,16 @@
 public class Conditions {
+
+    public static void main(String[] args) {
+
+        int salary = 20000;
+
+        if (salary <= 10000){
+            salary = salary + 2000;
+        } else if (salary <= 20000){
+            salary =  salary + 4000;
+        }
+
+        System.out.println(salary);
+    }
+
 }
